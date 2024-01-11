dc443ec89c35be4986c312f292a59d6609b460e9
serg988 <42653178+serg988@users.noreply.github.com>
2024-01-11 16:23:33 +0300

    completed, before refuctoring to class views and form model

--- todolist/models.py
+++ todolist/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Todo(models.Model):
     title = models.CharField(max_length=400)
     is_completed = models.BooleanField(default=False)
